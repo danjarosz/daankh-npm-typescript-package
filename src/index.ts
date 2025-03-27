a6e17983ce8a0e13baf37ae0f030faad63c26425
Daniel Jarosz <jarosz.daniel@protonmail.com>
2025-03-27 16:11:34 +0100

    fix: exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+// Import everything you need from a module
 import { add, subtract, multiply, divide } from './math.js';
 
-export default { add, subtract, multiply, divide };
+// Export functions modules separately
+export { add, subtract, multiply, divide };
